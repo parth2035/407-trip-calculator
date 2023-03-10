9c85021cf8557a585e264e64ca51eb9dd4603962
Parth Patel <parth.2035@gmail.com>
2023-03-09 21:02:44 -0500

    Added extra tests

--- tests/trip-calculator.test.js
+++ tests/trip-calculator.test.js
@@ -40,19 +40,21 @@ const {
     checkLocation
 } = require("../js/trip-calculator.js")
 
+const interchangeNames = getInterchangeNames(interchanges);
+
 /** costOfTrip function testing **/
 
-test('Checking distance of trip from QEW to Highway 401', async () => {
+test('Checking distance of trip from QEW to Highway 401 from costOfTrip function', async () => {
     const costDistArr = await costOfTrip(interchanges, "QEW", "Highway 401");
     expect(costDistArr[0].toFixed(4)).toBe("35.0120")
 })
 
-test('Checking distance of trip from Highway 401 to QEW', async () => {
+test('Checking distance of trip from Highway 401 to QEW from costOfTrip function', async () => {
     const costDistArr = await costOfTrip(interchanges, "Highway 401", "QEW");
     expect(costDistArr[0].toFixed(4)).toBe("35.0120")
 })
 
-test('Checking distance of trip from QEW to QEW', async () => {
+test('Checking distance of trip from QEW to QEW from costOfTrip function', async () => {
     const costDistArr = await costOfTrip(interchanges, "QEW", "QEW");
     expect(costDistArr[0].toFixed(4)).toBe("0.0000")
 })
@@ -74,6 +76,29 @@ test('Checking cost of trip from QEW to QEW', async () => {
 
 /** getDistance function testing **/
 
+test('Checking distance of trip from QEW to Highway 401 from getDistance function', async () => {
+    const distance = await getDistance(interchanges, interchangeNames, "QEW", "Highway 401");
+    expect(distance.toFixed(4)).toBe("35.0120")
+})
+
+test('Checking distance of trip from Sideline 22 to Salem Road from getDistance function', async () => {
+    const distance = await getDistance(interchanges, interchangeNames, "Sideline 22", "Salem Road");
+    expect(distance.toFixed(4)).toBe("7.3130")
+})
+
+// Expects to throw since it only counts up
+test('Checking distance of trip from Highway 401 to QEW from getDistance function', async () => {
+    expect(() => getDistance(interchanges, interchangeNames, "Highway 401", "QEW")).toThrow();
+})
+
 /** checkLocation function testing **/
 
+test('Checking existance of location Salem Road', async () => {
+    const locationExists = await checkLocation(interchangeNames, "Salem Road");
+    expect(locationExists).toBe(true);
+})
 
+test('Checking non-existance of location 407 ETR', async () => {
+    const locationExists = await checkLocation(interchangeNames, "407 ETR");
+    expect(locationExists).toBe(false);
+})
